bf4883530f081941556dec1631d288d4dad3d308
Hailey Keen <haileykeen@Haileys-MacBook-Pro.local>
2016-04-25 18:20:14 -0400

    removed http and put https for poster images

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,25 @@
-/**
- * Created by haileykeen on 4/21/16.
- */
+// grab our gulp packages
+var gulp  = require('gulp'),
+    jshint = require('gulp-jshint'),
+    sass   = require('gulp-sass');
+
+gulp.task('default', ['watch']);
+
+gulp.task('jshint', function() {
+  return gulp.src('public/**/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('build-css', function() {
+  return gulp.src('public/scss/**/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('public/stylesheets'));
+});
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+  gulp.watch('public/**/*.js', ['jshint']);
+  gulp.watch('public/scss/**/*.scss', ['build-css']);
+});
+
